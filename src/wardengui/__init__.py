19f043f18999202ba675bb5e1d8c255c75514384
Yehor(George) Shytikov <yshytikov@hammer.net>
2026-01-20 16:39:11 -0800

    Bump version to 1.0.12

--- src/wardengui/__init__.py
+++ src/wardengui/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "1.0.0"
 __author__ = "Yehor Shytikov"
 
-from .manager import WardenManager, CommandResult
+from .warden import WardenManager, CommandResult
+from .colors import Colors
 
-__all__ = ["WardenManager", "CommandResult"]
+__all__ = ["WardenManager", "CommandResult", "Colors"]
